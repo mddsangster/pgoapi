ee58d2d8e55f3012ff8bd14076efc41f6ac20866
Ryan Hope <rmh3093@gmail.com>
2016-08-01 07:44:24 -0400

    remove stray print

--- examples/bot/bot.py
+++ examples/bot/bot.py
@@ -222,8 +222,7 @@ class PoGoBot(object):
         time.sleep(delay)
 
     EARTH_RADIUS = 6371 * 1000
-    def get_cell_ids(self, lat, long, radius=1000):
-        print(lat,type(lat))
+    def get_cell_ids(self, lat, long, radius=1000
         # Max values allowed by server according to this comment:
         # https://github.com/AeonLucid/POGOProtos/issues/83#issuecomment-235612285
         if radius > 1500:
